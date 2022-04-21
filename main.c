422e2aa3da4d9ec9faf65c8921bf203a549d8e81
Ухин  Н.Е <support@cpkio.local>
2022-04-21 11:25:27 +0300

    Beta 1.2

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char **argv)
 {
-    char name[255];
+    char first[255], last[255];
 
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name));
-    name[strlen(name)-1] = '\0';
+    printf("Enter your first name: ");
+    fgets(first, 255, stdin);
+    first[strlen(first)-1] = '\0';
 
-    printf("Hello %s!\n", name);
+    printf("Now enter your last name: ");
+    gets(last);
+
+    printf("Hello %s %s!\n", first, last);
     return 0;
 }
+
